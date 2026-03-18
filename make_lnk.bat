bb70b9df0412cf5976bc6564ada48a0487977e4a
namakin13 <nofx.ynwa@gmail.com>
2026-03-18 10:53:25 +0900

    v1.1
    コードの最適化

--- make_lnk.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "$ws=New-Object -ComObject WScript.Shell;$s=$ws.CreateShortcut($env:USERPROFILE+'\Desktop\AudioSwitcher.lnk');$s.TargetPath='%~dp0start_app.bat';$s.WorkingDirectory='%~dp0';$s.Save()"
--- start_hidden.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run "cmd.exe /c start_app.bat", 0, False
